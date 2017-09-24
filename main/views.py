e2684de06c2bd4ee50af85d0447e2d6499c558cb
destiny07 <destiny.abellana7@gmail.com>
2017-09-24 13:47:43 +0800

    finalized version

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 import calendar
 import datetime
 
+import math
 from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
@@ -63,35 +64,121 @@ class ControlChartView(TemplateView):
         start_date = self.get_start_date(date)
         end_date = self.get_end_date(date)
 
+        # weekly_stat = self.get_weekly_job_stat(start_date, end_date)
+        # backjob_list = weekly_stat['backjob_list']
+        # total_job_list = weekly_stat['total_job_list']
+
+        backjob_list = [2, 3, 1, 3, 6, 6, 20]
+        total_job_list = [31, 25, 38, 28, 21, 25, 50]
+
+        dataset = self.generate_graph_data(backjob_list, total_job_list)
+        ucl = self.generate_graph_upper_control_limit(backjob_list, total_job_list)
+        cl = self.generate_graph_center_line(backjob_list, total_job_list)
+
         context['start_date'] = start_date
         context['end_date'] = end_date
-        context['data'] = self.generate_data(start_date, end_date)
+        context['dataset'] = dataset
+        context['ucl'] = ucl['dataset']
+        context['ucl_value'] = ucl['ucl_value']
+        context['cl'] = cl['dataset']
+        context['cl_value'] = cl['cl_value']
 
         return context
 
-    def generate_data(self, start_date, end_date):
+    def generate_graph_data(self, backjob_list, total_job_list):
+        dataset = []
+
+        # center_line = self.calculate_pbar(backjob_list, total_job_list)
+        # upper_control_limit = self.calculate_ucl(center_line, total_job_list[-1])
+
+        for i in range(0, len(total_job_list)):
+            pbar = self.calculate_pbar(backjob_list[i], total_job_list[i])
+
+            dataset.append({'x': i, 'y': pbar})
+
+        return dataset
+
+    def generate_graph_upper_control_limit(self, backjob_list, total_job_list):
+        pbar = self.calculate_pbar(backjob_list, total_job_list)
+        ucl = self.calculate_ucl(pbar, total_job_list[-1])
+        upper_control_limit_dataset = []
+
+        for i in range(0, len(total_job_list)):
+            upper_control_limit_dataset.append({'x': i, 'y': ucl})
+
+        return {'dataset': upper_control_limit_dataset, 'ucl_value': ucl}
+
+    def generate_graph_center_line(self, backjob_list, total_job_list):
+        center_line = self.calculate_pbar(backjob_list, total_job_list)
+        center_line_dataset = []
+
+        for i in range(0, len(total_job_list)):
+            center_line_dataset.append({'x': i, 'y': center_line})
+
+        return {'dataset': center_line_dataset, 'cl_value': center_line}
+
+    def calculate_ucl(self, pbar, total_job_count):
+        ucl = 0
+
+        try:
+            ucl = pbar + (3 * math.sqrt((pbar*(1-pbar))/total_job_count))
+        except ZeroDivisionError as e:
+            print(e)
+
+        return ucl
+
+    def calculate_pbar(self, backjob, total_job):
+        pbar = 0
+
+        if type(backjob) is list and type(total_job) is list:
+            backjob_count = 0
+            total_job_count = 0
+
+            for job in backjob:
+                backjob_count += job
+
+            for job in total_job:
+                total_job_count += job
+        else:
+            backjob_count = backjob
+            total_job_count = total_job
+
+        try:
+            pbar = backjob_count / total_job_count
+        except ZeroDivisionError as e:
+            print(e)
+
+        return pbar
+
+    def get_weekly_job_stat(self, start_date, end_date):
         curr_start_date = start_date
-
-        week_no = 1
-        previous_y = 0
-
-        data = []
+        backjob_list = []
+        total_job_list = []
 
         while curr_start_date < end_date:
             curr_end_date = curr_start_date + datetime.timedelta(days=6)
-            weekly_backjobs = self.get_back_job(curr_start_date, curr_end_date)
+            backjob_count = self.get_total_backjob(curr_start_date, curr_end_date)
+            total_job_count = self.get_total_job(curr_start_date, curr_end_date)
 
-            if weekly_backjobs:
-                proportion_of_backjob = self.calculate_weekly_score(weekly_backjobs)
-                previous_y = proportion_of_backjob
-            else:
-                proportion_of_backjob = previous_y
+            backjob_list.append(backjob_count)
+            total_job_list.append(total_job_count)
 
-            data.append({'x': week_no, 'y': proportion_of_backjob})
-            week_no += 1
-            curr_start_date += datetime.timedelta(days=7)
+            curr_start_date += datetime.timedelta(days=6)
 
-        return data
+        return {'backjob_list': backjob_list, 'total_job_list': total_job_list}
+
+    def get_total_backjob(self, start_date, end_date):
+        total_job = JobOrder.objects.filter(datetime_filed__range=[start_date, end_date])
+        backjob_count = 0
+
+        for job in total_job:
+            if job.is_job_reworked:
+                backjob_count += 1
+
+        return backjob_count
+
+    def get_total_job(self, start_date, end_date):
+        return JobOrder.objects.filter(datetime_filed__range=[start_date, end_date]).count()
 
     def get_start_date(self, date):
         weekday = date.weekday()
@@ -108,26 +195,6 @@ class ControlChartView(TemplateView):
         end_date = first_weekday_date + datetime.timedelta(days=6)
 
         return end_date
-
-    def calculate_weekly_score(self, weekly_backjobs):
-        num_of_yes = 0
-        num_of_no = 0
-
-        for backjob in weekly_backjobs:
-            if backjob.is_job_reworked:
-                num_of_yes += 1
-            else:
-                num_of_no += 1
-
-        try:
-            proportion_of_backjob = num_of_yes / (num_of_yes + num_of_no)
-        except ZeroDivisionError:
-            proportion_of_backjob = 0
-
-        return proportion_of_backjob
-
-    def get_back_job(self, start_date, end_date):
-        return JobOrder.objects.filter(datetime_filed__range=[start_date, end_date])
 
     def get_date_range(self, date):
         return (date - datetime.timedelta(6*365/12))
@@ -153,7 +220,7 @@ class MonthlyBackJobPieChart(TemplateView):
         data = self.generate_data(int(year), int(month))
 
         context['year'] = year
-        context['month'] = month
+        context['month'] = calendar.month_name[int(month)]
         context['backjob_score'] = data['backjob']
         context['non_backjob_score'] = data['non_backjob']
 
@@ -170,16 +237,22 @@ class MonthlyBackJobPieChart(TemplateView):
         weekday_difference = 6 - weekday_month_last_date
         end_date = month_last_date + datetime.timedelta(days=weekday_difference)
 
-        backjob_list = self.get_backjob(start_date, end_date)
-        backjob_count = 0
+        job_count = self.get_total_job(start_date, end_date)
+        backjob_count = self.get_total_backjob(start_date, end_date)
 
-        for backjob in backjob_list:
-            if backjob.is_job_reworked:
-                backjob_count += 1
-
-        non_backjob_count = backjob_list.count() - backjob_count
+        non_backjob_count = job_count - backjob_count
 
         return {'backjob': backjob_count, 'non_backjob': non_backjob_count}
 
-    def get_backjob(self, start_date, end_date):
-        return JobOrder.objects.filter(datetime_filed__range=[start_date, end_date])
+    def get_total_backjob(self, start_date, end_date):
+        total_job_list = JobOrder.objects.filter(datetime_filed__range=[start_date, end_date])
+        backjob_count = 0
+
+        for backjob in total_job_list:
+            if backjob.is_job_reworked:
+                backjob_count += 1
+
+        return backjob_count
+
+    def get_total_job(self, start_date, end_date):
+        return JobOrder.objects.filter(datetime_filed__range=[start_date, end_date]).count()
